3668c9255af5bc82d278a7f62e0039f5e1fc8051
Mansi Kerai <mansipatel0080@gmail.com>
2026-01-21 14:34:09 +0530

    Initial commit - changes made in wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from products.models import Product
@@ -9,14 +10,24 @@ def wishlist_detail(request):
     return render(request, 'wishlist/wishlist.html', {'items': items})
 
 
+
 @login_required(login_url='login')
 def add_to_wishlist(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    Wishlist.objects.get_or_create(
-        user=request.user,
-        product=product
-    )
-    return redirect('wishlist')
+    if request.method == "POST":
+        product = get_object_or_404(Product, id=product_id)
+
+        Wishlist.objects.get_or_create(
+            user=request.user,
+            product=product
+        )
+
+        return JsonResponse({
+            "status": "success",
+            "message": "Added to wishlist"
+        })
+
+    return JsonResponse({"status": "error"}, status=400)
+
 
 
 @login_required(login_url='login')
